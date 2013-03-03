a3bb76aea05b2bd86349e323be409c0b5490ae3b
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-02 19:03:12 -0500

    Pseudo working Post model. Missing features.
    Missing features such as a query for many Posts, such that we could
    find out what's already existing.

--- db/post.go
+++ db/post.go
@@ -7,8 +7,33 @@ import (
 	"time"
 )
 
+//
+// SQL queries
+//
+var createTable string = `
+CREATE TABLE IF NOT EXISTS Posts(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   author VARCHAR(255),
+   content TEXT,
+   date TIMESTAMP
+)`
+
+var insertOrReplaceRowForId string = `
+INSERT OR REPLACE INTO Posts( author, content, date)
+VALUES( ?, ?, ?)
+`
+var findRowById string = `
+SELECT P.author, P.content, P.date
+FROM Posts AS P
+WHERE P.id = ?`
+
+var deleteRowById string = `
+DELETE FROM Posts
+WHERE Posts.id = ?`
+
+// Represents a post in the blog
 type Post struct {
-	id      int
+	id      int64
 	author  string
 	content string
 	date    time.Time
@@ -22,17 +47,9 @@ func init() {
 	}
 	defer db.Close()
 
-	createTable := `
-   CREATE TABLE Posts(
-      id INTEGER NOT NULL PRIMARY KEY,
-      author VARCHAR(255),
-      content TEXT,
-      date TIMESTAMP,
-   )`
-
 	_, err = db.Exec(createTable)
 	if err != nil {
-		fmt.Println("Error creating Posts table", err)
+		fmt.Println("Error creating Posts table:", err)
 		return
 	}
 
@@ -47,16 +64,42 @@ func NewPost(author, content string) *Post {
 	return p
 }
 
-func FindPostByID(id int) Post {
+// Finds a post that match the given id
+func FindPostByID(id int64) (Post, error) {
+	var p Post
+
 	db, err := sql.Open(DBDriver(), DBName())
 	if err != nil {
 		fmt.Println("FindPostByID", err)
-		return
+		return p, err
 	}
 	defer db.Close()
 
+	stmt, err := db.Prepare(findRowById)
+	if err != nil {
+		fmt.Println("FindPostByID", err)
+		return p, err
+	}
+	defer stmt.Close()
+
+	var author string
+	var content string
+	var date time.Time
+	err = stmt.QueryRow(id).Scan(&author, &content, &date)
+	if err != nil {
+		fmt.Println("FindPostByID", err)
+		return p, err
+	}
+
+	p.id = id
+	p.author = author
+	p.content = content
+
+	return p, nil
 }
 
+// Saves the post (or update it if it already exists)
+// to the database
 func (p *Post) Save() {
 	db, err := sql.Open(DBDriver(), DBName())
 	if err != nil {
@@ -64,8 +107,24 @@ func (p *Post) Save() {
 		return
 	}
 	defer db.Close()
+
+	stmt, err := db.Prepare(insertOrReplaceRowForId)
+	if err != nil {
+		fmt.Println("Save", err)
+		return
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(p.author, p.content, p.date)
+	if err != nil {
+		fmt.Println("Save", err)
+		return
+	}
+
+	p.id, _ = res.LastInsertId()
 }
 
+// Deletes the post from the database
 func (p *Post) Destroy() {
 
 	db, err := sql.Open(DBDriver(), DBName())
@@ -75,12 +134,7 @@ func (p *Post) Destroy() {
 	}
 	defer db.Close()
 
-	deleteQuery := `
-   DELETE FROM Posts
-   WHERE Post.id = ?
-   `
-
-	stmt, err := db.Prepare(deleteQuery)
+	stmt, err := db.Prepare(deleteRowById)
 	if err != nil {
 		fmt.Println("Destroy", err)
 		return
--- db/sqlite_db.go
+++ db/sqlite_db.go
@@ -1,12 +1,5 @@
 package db
 
-import (
-	"database/sql"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"os"
-)
-
 func DBName() string {
 	return "./goblog.db"
 }
@@ -16,109 +9,13 @@ func DBDriver() string {
 }
 
 func Start() {
-	os.Remove(DBName())
 
-	db, err := sql.Open(DBDriver(), DBName())
+	p := NewPost("Antoine", "Il était une fois")
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
+	p.Save()
 
-	sqls := []string{
-		"CREATE TABLE Posts (id integer not null primary key, content text)",
-		"DELETE FROM Posts",
-	}
-
-	for _, sql := range sqls {
-		_, err = db.Exec(sql)
-		if err != nil {
-			fmt.Printf("%q: %s\n", err, sql)
-			return
-		}
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	stmt, err := tx.Prepare("INSERT INTO Posts(id, content) VALUES(?, ?)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer stmt.Close()
-
-	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-	tx.Commit()
-
-	rows, err := db.Query("SELECT id, content FROM Posts")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var content string
-		rows.Scan(&id, &content)
-		fmt.Println(id, content)
-	}
-	rows.Close()
-
-	stmt, err = db.Prepare("SELECT content FROM Posts WHERE id = ?")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer stmt.Close()
-
-	var content string
-	err = stmt.QueryRow("3").Scan(&content)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(content)
-
-	_, err = db.Exec("DELETE FROM Posts")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	_, err = db.Exec("INSERT INTO Posts(id, content) VALUES(1, 'Hello world'), (2, 'Baromètre'), (3, 'Élucubrationization')")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	rows, err = db.Query("SELECT id, content FROM Posts")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var content string
-		rows.Scan(&id, &content)
-		fmt.Print(id, content)
-	}
-	rows.Close()
+	p2 := NewPost("John Dow", "Lalalalal")
+	p2.Save()
+	p2.Destroy()
 
 }
