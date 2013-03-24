83f317cc66390acf5a5b1e6dfd8c4bd8d1108e43
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-24 18:13:35 -0400

    Fix the fmt bug.

--- ctlr/index_controller.go
+++ ctlr/index_controller.go
@@ -19,7 +19,7 @@ func (i *index) Path() string {
 func (i *index) Controller(conn *db.DBConnection) func(http.ResponseWriter, *http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 
-		//
+		fmt.Fprintf(rw, "this is the index page!")
 
 	}
 }
