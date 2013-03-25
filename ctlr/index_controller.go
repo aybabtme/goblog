47c8c3cf72a7d35893cba76de75ad65f53378447
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-24 21:25:54 -0400

    Fix some formatting.

--- ctlr/index_controller.go
+++ ctlr/index_controller.go
@@ -16,7 +16,8 @@ func (i *index) Path() string {
 	return "/"
 }
 
-func (i *index) Controller(conn *db.DBConnection) func(http.ResponseWriter, *http.Request) {
+func (i *index) Controller(conn *db.DBConnection) func(http.ResponseWriter,
+	*http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 
 		fmt.Fprintf(rw, "this is the index page!")
