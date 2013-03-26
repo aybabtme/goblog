f49076cbfec71ab64a802d457257149c340022f3
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-26 00:46:12 -0400

    More decent random data generation.

--- goblog.go
+++ goblog.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/aybabtme/goblog/model"
 	"github.com/aybabtme/gypsum"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -51,78 +53,56 @@ func setupDatabase(modelurl string) (*model.DBConnection, error) {
 }
 
 func generateData(conn *model.DBConnection) error {
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	user1 := conn.NewUser(
-		"Antoine Grondin",
-		time.Now().UTC(),
-		-5,
-		"google+",
-		"heheveb7673tygvh23",
-		"antoinegrondin@gmail.com")
-	author := conn.NewAuthor(user1)
-	if err := author.Save(); nil != err {
-		return err
-	}
-	post1 := conn.NewPost(author,
-		"Il était une fois",
-		gypsum.Lorem(),
-		"http://media.zoom-cinema.fr/photos/news/2380/il-etait-une-fois-2007-4.jpg",
-		time.Now().UTC())
-	if err := post1.Save(); nil != err {
-		return err
-	}
-	if _, err := post1.AddLabel("prince"); err != nil {
-		return err
-	}
-	if _, err := post1.AddLabel("princess"); err != nil {
-		return err
-	}
-	if _, err := post1.AddLabel("drama"); err != nil {
-		return err
-	}
+	postCount := rand.Intn(20) + 4
+	for i := 0; i < postCount; i++ {
+		user := conn.NewUser(
+			strings.Title(gypsum.WordLorem(2)),
+			time.Now().UTC(),
+			-5,
+			strings.Title(gypsum.WordLorem(1)),
+			strings.Title(gypsum.WordLorem(5)),
+			strings.Title(gypsum.WordLorem(5)))
+		author := conn.NewAuthor(user)
+		if err := author.Save(); nil != err {
+			return err
+		}
+		titleCount := rand.Intn(4) + 3
+		paraCount := rand.Intn(6) + 3
+		post := conn.NewPost(author,
+			strings.Title(gypsum.WordLorem(titleCount)),
+			gypsum.ArticleLorem(paraCount, "\n"),
+			"http://media.zoom-cinema.fr/photos/news/2380/il-etait-une-fois-2007-4.jpg",
+			time.Now().UTC())
+		if err := post.Save(); nil != err {
+			return err
+		}
+		labelCount := rand.Intn(2) + 1
+		for j := 0; j < labelCount; j++ {
+			if _, err := post.AddLabel(gypsum.WordLorem(1)); err != nil {
+				return err
+			}
+		}
 
-	post2 := conn.NewPost(author,
-		"Parenthood",
-		gypsum.Lorem(),
-		"http://www.blessedquietness.com/STUPID01.jpg",
-		time.Now().UTC())
-	if err := post2.Save(); nil != err {
-		return err
-	}
+		commentCount := rand.Intn(10)
+		for k := 0; k < commentCount; k++ {
+			commenter := conn.NewUser(
+				strings.Title(gypsum.WordLorem(2)),
+				time.Now().UTC(),
+				-5,
+				strings.Title(gypsum.WordLorem(1)),
+				strings.Title(gypsum.WordLorem(5)),
+				strings.Title(gypsum.WordLorem(5)))
+			commenter.Save()
+			conn.NewComment(commenter.Id(),
+				post.Id(),
+				gypsum.Lorem(),
+				time.Now().UTC()).
+				Save()
+		}
 
-	if _, err := post2.AddLabel("childhood"); err != nil {
-		return err
 	}
-	if _, err := post2.AddLabel("funny"); err != nil {
-		return err
-	}
-
-	user2 := conn.NewUser(
-		"John Smith",
-		time.Now().UTC(),
-		-5,
-		"google+",
-		"fdvh23",
-		"ajfdsin@gmail.com")
-	user2.Save()
-	user3 := conn.NewUser(
-		"Chris Poirier",
-		time.Now().UTC(),
-		-5,
-		"google+",
-		"fdvh23",
-		"ajf21vbnlkdsin@gmail.com")
-	user3.Save()
-
-	conn.NewComment(user1.Id(), post1.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user2.Id(), post1.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user3.Id(), post1.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user1.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user2.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user3.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user1.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user2.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
-	conn.NewComment(user3.Id(), post2.Id(), gypsum.Lorem(), time.Now()).Save()
 
 	return nil
 }
