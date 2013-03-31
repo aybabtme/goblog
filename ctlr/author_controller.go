15e8913549e6df637b49b56ef2030e3886c2cf13
VoiiD <christopherpoirier1@gmail.com>
2013-03-31 11:09:47 -0400

    Add some template generation for author.

--- ctlr/author_controller.go
+++ ctlr/author_controller.go
@@ -1,27 +1,106 @@
 package ctlr
 
 import (
+	"fmt"
 	"github.com/aybabtme/goblog/model"
+	"github.com/aybabtme/goblog/view"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
+	"strconv"
 )
 
+type author struct {
+	path string
+	view *template.Template
+}
+
+type data struct {
+	AllPosts  []model.Post
+	AllLabels []model.Label
+}
+
 func NewAuthorController() Controller {
-	return new(author)
+	var a author
+	a.path = "/author"
+	a.view = view.GetAuthorListingTemplate()
+	return a
 }
 
-type author string
-
-func (a *author) Path() string {
-	return "/author/{key}"
+func NewAuthorIdController() Controller {
+	var a author
+	a.path = "/author/{id:[0-9]+}"
+	a.view = view.GetAuthorTemplate()
+	return a
 }
 
-func (a *author) Controller(conn *model.DBConnection) func(http.ResponseWriter, *http.Request) {
+func (a author) Path() string {
+	return a.path
+}
+
+func (a author) Controller(conn *model.DBConnection) func(http.ResponseWriter,
+	*http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
-		_ = vars["key"]
+		id := vars["id"]
 
-		// dispatch with conn and rw, req
-
+		if id == "" {
+			a.authorIndex(conn, rw, req)
+		} else {
+			a.authorPosts(conn, rw, req, id)
+		}
 	}
+}
+
+func (a *author) authorIndex(conn *model.DBConnection,
+	rw http.ResponseWriter,
+	req *http.Request) {
+
+	authors, err := conn.FindAllAuthors()
+	if err != nil {
+		fmt.Println("authorController for listing 1:", err)
+		return
+	}
+	if err := a.view.Execute(rw, authors); nil != err {
+		fmt.Println("authorController for listing 2:", err)
+		return
+	}
+}
+
+func (a *author) authorPosts(conn *model.DBConnection,
+	rw http.ResponseWriter,
+	req *http.Request,
+	id string) {
+
+	intId, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		fmt.Println("AuthorController for Posts: ", err)
+		return
+	}
+	author, err := conn.FindAuthorById(intId)
+	if err != nil {
+		fmt.Println("AuthorController for Posts: ", err)
+		return
+	}
+	posts, err := author.Posts()
+	if err != nil {
+		fmt.Println("AuthorController for Posts: ", err)
+		return
+	}
+	labels, err := conn.FindAllLabels()
+	if err != nil {
+		fmt.Println("AuthorController for Posts: ", err)
+		return
+	}
+
+	d := data{
+		AllPosts:  posts,
+		AllLabels: labels,
+	}
+
+	if err := a.view.Execute(rw, d); nil != err {
+		fmt.Println("AuthorController for Posts: ", err)
+		return
+	}
+
 }
